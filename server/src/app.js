8b830488534662865842bc405d914111e54217ca
Sergej Karavajev <job@vprage.info>
2018-08-13 18:54:04 +0200

    Incorrect JSON handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const logger = require('morgan')
 const path = require('path')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const converterRoutes = require('./routes/converter-routes')
 
 const app = express()
 
 app.use(logger('dev'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use((err, req, res, next) => {
+	if (err) {
+		res.statusCode = 400
+		res.json('Invalid Request data')
+	} else {
+		next()
+	}
+})
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('../../app'))
-app.use(converterRoutes);
+app.use(converterRoutes)
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../../app', 'index.html'))
